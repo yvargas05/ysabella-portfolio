4f681b3a27e128809edd587914da90c0689d5e1c
yvargas05 <ysabellavargas05@gmail.com>
2025-08-06 15:47:43 -0400

    new project

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -24,6 +24,23 @@ export default function Projects() {
             </a>
           </li>
 
+          <li className="border rounded-xl p-10 bg-white/80 shadow hover:shadow-2xl transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 project-hover">
+            <h2 className="text-2xl font-bold leading-10 font-serif">
+              TransMedics Internship Expierience, Summer 2025
+            </h2>
+            <p className="text-base font-normal font-serif">
+              Created a web page application for my end of internship presentation at TransMedics describing the projects I worked on, skills I learned, and my career development during my internship. The page is designed to be visually appealing and informative, showcasing my growth and contributions, and it was made using React, Tailwind CSS, and Next.js.
+            </p>
+            <a
+              href="https://github.com/yvargas05/transmedics-internship"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline leading-10 font-serif"
+            >
+              View on GitHub
+            </a>
+          </li>
+
           {/* Add more projects here */}
         </ul>
       </main>
